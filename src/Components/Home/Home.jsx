2cabf923f30b33bfd2157066cab39683f745f659
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-05-07 13:16:16 +0200

    home upd

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,35 @@
 import React from "react";
 import "./Home.css";
+import { Col, Container, Row } from "react-bootstrap";
+
+//import
+import Homeimg from "../../Assets/Img/home.png";
+import Homeimg2 from "../../Assets/Img/home2.png";
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <Container fluid className="home-container">
+        <Row>
+          <Col xs={12}>
+            <div className="card-home position-relative">
+              <img className="first" src={Homeimg} alt="" />
+              <img src={Homeimg2} className="second" alt="" />
+              <div className="info position-absolute d-flex flex-column justify-content-between ">
+                <div className="mt-4">
+                  <h1 className="text-center ">Titolo</h1>
+                  <h2 className="ms-4 mt-4">Data</h2>
+                  <h2 className="ms-4">Artista</h2>
+                </div>
+                <div className="mb-4">
+                  <h3 className="ms-2">Album:</h3>
+                  <h3 className="ms-2">Rating:</h3>
+                  <h3 className="ms-2">Hidden Gem:</h3>
+                </div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
